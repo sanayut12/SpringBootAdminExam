ef2eeacbf6b4bdfb745bbcb3ec7e58acdbff3072
Mali0210 <56685977+sanayut12@users.noreply.github.com>
2022-07-21 11:48:39 +0700

    add ex1 ,admin client1 client2 client3

---
+++ ex1/client3/src/test/java/com/demo/client3/Client3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.client3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Client3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
